360ad0425eaf8db5358367d8f1003857f0ae810e
David McCullough <davidm@snapgear.com>
2007-10-11 03:50:51 +0000

    the intl subdir in binutils may redirect some funcs with libintl_* prefixes (like building for win32), so the attached patch adds a stub like the existing one for dcgettect() ... this fixes building of elf2flt for me for mingw targets
    Mike Frysinger <vapier@gentoo.org>

--- stubs.c
+++ stubs.c
@@ -4,3 +4,10 @@ const char *dcgettext (const char *domain, const char *msg, int category)
   return msg;
 }
 #endif /* !HAVE_DCGETTEXT */
+
+#ifndef HAVE_LIBINTL_DGETTEXT
+const char *libintl_dgettext (const char *domain, const char *msg)
+{
+  return msg;
+}
+#endif /* !HAVE_LIBINTL_DGETTEXT */
